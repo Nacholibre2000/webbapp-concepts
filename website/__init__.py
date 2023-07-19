20e4ff97227a46dd65d07cb057416ec64e0ca5c6
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-07-19 10:15:32 +0000

    login connectivity to database achieved

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from os import path
 import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY"),
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
     # Create engine
     engine = create_engine(f'mysql+mysqlconnector://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}')
@@ -19,7 +19,7 @@ def create_app():
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 10, 'max_overflow': 20}
     
     db.init_app(app)
-
+  
     from .views import views
     from .auth import auth
 
@@ -29,6 +29,14 @@ def create_app():
     from .models import Users, Concepts
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+      return Users.query.get(int(id))
 
     return app
 
