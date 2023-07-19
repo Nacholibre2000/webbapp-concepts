4a773148b4ff15f134d00c6d9c5a8dc82b6d0e98
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-07-19 19:13:29 +0000

    semi functional concepts input field achieved

--- website/views.py
+++ website/views.py
@@ -1,9 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Concepts
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-  return render_template("home.html")
+  if request.method == 'POST':
+    concept = request.form.get('concept')
+
+    if len(concept) < 1:
+      flash('Concept is too short!', category='error')
+    else:
+      new_concept = Concepts(concept=concept, user_id=current_user.id)
+      db.session.add(new_concept)
+      db.session.commit()
+      flash('Concept added!', category='success')
+      
+  return render_template("home.html", user=current_user)
