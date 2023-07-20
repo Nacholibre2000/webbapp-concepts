c5cd012e25a266b622a416ba5e6f3ba6402f103f
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-07-20 09:21:15 +0000

    full note functionality achieved

--- website/models.py
+++ website/models.py
@@ -2,16 +2,15 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Users(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    concepts = db.relationship('Concepts')
+    notes = db.relationship('Note')
 
-class Concepts(db.Model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    concept = db.Column(db.String(100))
-    explanation = db.Column(db.String(1000))
+    data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
