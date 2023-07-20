e4cb5e94987b942a2ffd6d7b7b89fc1781c63700
Nacholibre2000 <133748645+Nacholibre2000@users.noreply.github.com>
2023-07-20 10:51:03 +0000

    working renaming of note to concepts

--- website/models.py
+++ website/models.py
@@ -7,10 +7,10 @@ class Users(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    user_concepts = db.relationship('Concepts')
 
-class Note(db.Model):
+class Concepts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    concept = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Concepts
 from . import db
 import json
 
@@ -16,7 +16,7 @@ def home():
         if len(note) < 1:
             flash('Note is too short!', category='error') 
         else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+            new_note = Concepts(concept=note, user_id=current_user.id)  #providing the schema for the note 
             db.session.add(new_note) #adding the note to the database 
             db.session.commit()
             flash('Note added!', category='success')
@@ -28,7 +28,7 @@ def home():
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
     noteId = note['noteId']
-    note = Note.query.get(noteId)
+    note = Concepts.query.get(noteId)
     if note:
         if note.user_id == current_user.id:
             db.session.delete(note)
