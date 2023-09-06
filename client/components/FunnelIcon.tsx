c6e93106c40d5e5524a29ccf7aa271c66886090a
Nacholibre2000 <jonatan.blomberg20@gmail.com>
2023-09-06 21:26:10 +0200

    icon now has three states

--- client/components/FunnelIcon.tsx
+++ client/components/FunnelIcon.tsx
@@ -3,9 +3,11 @@ type FunnelIconProps = {
 };
 
 const FunnelIcon: React.FC<FunnelIconProps> = ({ status }) => {
-  // Define colors based on the status
+  
+  const detoggledColor = '#545454';
   const fillColor = status === 'toggled' ? '#9CA3AF' : 'transparent';
-  const strokeColor = status === 'half-toggled' ? '#9CA3AF' : '#474747';
+  const strokeColor = status === 'half-toggled' ? '#9CA3AF' : detoggledColor;
+
 
   return (
     <svg width="24" height="24" viewBox="0 0 348 313" xmlns="http://www.w3.org/2000/svg">
@@ -14,7 +16,7 @@ const FunnelIcon: React.FC<FunnelIconProps> = ({ status }) => {
         <g transform="matrix(0.538813,0,0,0.600673,257.571,152.475)">
           <path 
             d="M176.721,14.948C99.75,14.948 37.26,77.438 37.26,154.408C37.26,231.379 99.75,293.869 176.721,293.869C253.691,293.869 316.181,231.379 316.181,154.408C316.181,77.438 253.691,14.948 176.721,14.948ZM92.744,85.495C88.489,85.495 84.581,87.845 82.586,91.604C80.591,95.363 80.835,99.916 83.22,103.44L144.123,193.437C144.123,193.437 144.481,225.699 144.481,225.699C144.531,230.23 147.237,234.309 151.392,236.117L200.124,257.322C203.679,258.869 207.774,258.52 211.017,256.395C214.259,254.27 216.213,250.654 216.213,246.777L216.213,193.487C216.213,193.487 270.558,102.912 270.558,102.912C272.69,99.359 272.745,94.934 270.704,91.329C268.663,87.724 264.84,85.495 260.697,85.495L92.744,85.495Z"
-            fill={fillColor}
+            fill={status === 'detoggled' ? detoggledColor : fillColor}
           />
         </g>
         <g transform="matrix(0.791045,0,0,0.881863,69.9937,45.3114)">
